2bf53d2aac2997cfa7359acfa80a26b3a5a5678b
Jame Ekene <edehjamesraphael@gmail.com>
2023-04-01 11:20:55 +0100

    edited readme file

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Link to the live app
+
+[Live app](https://custom-scheduler.netlify.app/)
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/utils/dummyData.ts
+++ src/utils/dummyData.ts
@@ -1,13 +1,13 @@
-//MM:DD:YY
+// Date format is : MM:DD:YY
 
 export const apiData = [
   {
     id: 1,
     title: "Things Fall Apart",
-    startDate: new Date("3/30/2023"),
-    endDate: new Date("3/30/2023"),
-    startTime: "19:00",
-    endTime: "19:45",
+    startDate: new Date("4/1/2023"),
+    endDate: new Date("4/1/2023"),
+    startTime: "7:00",
+    endTime: "8:45",
     clientImg:
       "/imgs/dog-golden-retriever-jumping-autumn-leaves-autumnal-sunlight-77861618.jpg",
     clientName: "Ajuri Alaka",
@@ -18,10 +18,10 @@ export const apiData = [
   {
     id: 2,
     title: "Half of a Yellow Sun",
-    startDate: new Date("3/22/2023"),
-    endDate: new Date("3/22/2023"),
-    startTime: "19:00",
-    endTime: "20:00",
+    startDate: new Date("5/1/2023"),
+    endDate: new Date("5/1/2023"),
+    startTime: "9:00",
+    endTime: "10:00",
     clientImg: "/imgs/images.jpg",
     clientName: "Janet Dope",
     appointmentWith: "Dr.Justin Land",
@@ -31,15 +31,28 @@ export const apiData = [
   {
     id: 3,
     title: "Purple Hibiscus",
-    startDate: new Date("3/22/2023"),
-    endDate: new Date("3/22/2023"),
-    startTime: "20:00",
-    endTime: "23:30",
+    startDate: new Date("4/6/2023"),
+    endDate: new Date("4/6/2023"),
+    startTime: "8:00",
+    endTime: "9:00",
     clientImg: "/imgs/istockphoto-1191962502-612x612.jpg",
     clientName: "Jon Doe",
-    appointmentWith: "Dr.Samie Ejor",
-    appointmentWithId: "dc-1",
+    appointmentWith: "Dr.Janet Jolls",
+    appointmentWithId: "dc-3",
     appointmentWithImg:
       "/imgs/two-cute-golden-retriever-puppies-playing-photo-45116795.jpg",
+  },
+  {
+    id: 4,
+    title: "There was a country",
+    startDate: new Date("4/3/2023"),
+    endDate: new Date("4/3/2023"),
+    startTime: "9:00",
+    endTime: "10:30",
+    clientImg: "/imgs/istockphoto-1191962502-612x612.jpg",
+    clientName: "Asa Nwa",
+    appointmentWith: "Dr.Samie Ejor",
+    appointmentWithId: "dc-1",
+    appointmentWithImg: "/imgs/images (1).jpg",
   },
 ];
